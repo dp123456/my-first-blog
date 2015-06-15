f2ecac179878c4422e3f1512613485e3f6c4977f
Paul127 <pauliusx127@gmail.com>
2015-06-15 14:04:36 +0300

    add / edit post

---
+++ blog/migrations/0004_auto_20150615_1327.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20150615_1234'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='author',
+            field=models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
---
+++ blog/migrations/0005_auto_20150615_1333.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0004_auto_20150615_1327'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='author',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, default=1),
+            preserve_default=False,
+        ),
+    ]
